fdd95ec7f22afe180421ce53ed21670c7b2867d1
Shubham Berkar <Sberkar12@gmail.com>
2023-04-03 18:40:31 +0530

    production backend added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api"
+const BASE_URL = "https://kaam-backend.cyclic.app/api";
 
 export default axios.create({
-    baseURL: BASE_URL
-})
+  baseURL: BASE_URL,
+});
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        "Content-Type": "Application/json"
-    },
-    withCredentials: true
-})
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "Application/json",
+  },
+  withCredentials: true,
+});
